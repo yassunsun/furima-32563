575153aba051ff1ae136b27180583d392e908f9f
yassunsun <ww16471z@icloud.com>
2020-12-13 02:50:21 +0900

    テーブルへの保存許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
-end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+ 
+  def configure_permitted_parameters
+    binding.pry
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :birthday, :first_name_kanji, :last_name_kanji, :first_name_katakana, :last_name_kataname]
+    )
+  end
+ end
