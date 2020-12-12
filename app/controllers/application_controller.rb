011fc2623ba689df9cba268d4e79854ba0fc25e5
yassunsun <ww16471z@icloud.com>
2020-12-13 02:53:09 +0900

    Revert "テーブルへの保存許可"
    This reverts commit 575153aba051ff1ae136b27180583d392e908f9f.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
- 
-  def configure_permitted_parameters
-    binding.pry
-    devise_parameter_sanitizer.permit(
-      :sign_up, keys: [:nickname, :email, :birthday, :first_name_kanji, :last_name_kanji, :first_name_katakana, :last_name_kataname]
-    )
-  end
- end
+end
