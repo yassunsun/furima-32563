e05f1b8e1cdf5bde4ce84f327bb7b5e8108646a9
yassunsun <ww16471z@icloud.com>
2020-12-13 15:33:27 +0900

    FactoryBotの編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {"test"}
+    email                 {"test@example"}
+    password              {"000000"}
+    password_confirmation {password}
+    last_name_kanji
+    first_name_kanji
+    last_name_katakana
+    first_name_katakana
+    birthday
   end
 end
