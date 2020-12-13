f6a7de824e935815d3d55aa50375199145181ee9
yassunsun <ww16471z@icloud.com>
2020-12-13 19:22:51 +0900

    emailの登録できない場合の追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,6 +55,18 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include "Email can't be blank"
       end
+      it "重複したemailが存在する場合登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it "emailは@を含まないと登録できない" do
+        @user.email = "aaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email is invalid"
+      end
       it "passwordが空では登録できない" do
         @user.password = ""
         @user.valid?
