360be9c2b1fac3be7de5c7a508a0b2d7261353dd
yassunsun <ww16471z@icloud.com>
2020-12-14 11:39:28 +0900

    半角英数字passwordの自動生成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"a1" + Faker::Internet.password(min_length: 4)}
     password_confirmation {password}
     last_name_kanji       {"安本"}
     first_name_kanji      {"悠人"}
