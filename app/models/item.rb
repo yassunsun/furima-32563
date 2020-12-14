028214de5cca5a2a0b91b6d409e0448b15b69174
yassunsun <ww16471z@icloud.com>
2020-12-14 18:57:41 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :day_id
+    validates :price
+    validates :image
+  end
 end
