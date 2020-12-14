3da1cc7a71ee8b8a318b8130c4bbae47b2d11122
yassunsun <ww16471z@icloud.com>
2020-12-14 18:30:25 +0900

    ストロングパラメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,10 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  private
+  
+  def prototype_params
+    params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :fee_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
