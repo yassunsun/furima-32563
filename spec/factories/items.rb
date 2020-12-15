9a36cfbf35f2f4061517099bb6efc1baaaae69ca
yassunsun <ww16471z@icloud.com>
2020-12-15 23:08:15 +0900

    FactoryBotの生成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    title         { 'テスト' }
+    explanation   { 'テストです' }
+    category_id   { '2' }
+    condition_id  { '2' }
+    fee_id        { '2' }
+    prefecture_id { '2' }
+    day_id        { '2' }
+    price         { '2000' }
   end
 end
