b0194bb25885cdd4a4152b1c02e3259296d1bccf
yassunsun <ww16471z@icloud.com>
2020-12-16 13:06:16 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,44 +9,8 @@ RSpec.describe Item, type: :model do
       it 'image, title, explanation, category_id, condition_id, fee_id, prefecture_id, day_id, priceが存在すれば出品できる' do
         expect(@item).to be_valid
       end
-      it '画像が存在すれば出品できる' do
-        @item.image = fixture_file_upload('public/images/test_image.png')
-        expect(@item).to be_valid
-      end
-      it '商品名が存在すれば出品できる' do
-        @item.title = 'テスト'
-        expect(@item).to be_valid
-      end
-      it '商品の説明が存在すれば出品できる' do
-        @item.explanation = 'テストです'
-        expect(@item).to be_valid
-      end
-      it 'カテゴリーが存在すれば出品できる' do
-        @item.category_id = 2
-        expect(@item).to be_valid
-      end
-      it '商品の状態が存在すれば出品できる' do
-        @item.condition_id = 2
-        expect(@item).to be_valid
-      end
-      it '配送料の負担が存在すれば出品できる' do
-        @item.fee_id = 2
-        expect(@item).to be_valid
-      end
-      it '発送元の地域が存在すれば出品できる' do
-        @item.prefecture_id = 2
-        expect(@item).to be_valid
-      end
-      it '発送までの日数が存在すれば出品できる' do
-        @item.day_id = 2
-        expect(@item).to be_valid
-      end
-      it '販売価格が存在すれば出品できる' do
-        @item.price = 2000
-        expect(@item).to be_valid
-      end
     end
-
+      
     context '商品出品がうまくいかないとき' do
       it 'imageが空だと出品できない' do
         @item.image = nil
