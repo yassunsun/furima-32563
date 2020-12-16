930a55d710b096a3abd18222bf709a9e4c26f41f
yassunsun <ww16471z@icloud.com>
2020-12-16 11:57:01 +0900

    with_optionsの使用

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,12 +17,15 @@ class Item < ApplicationRecord
     validates :price
   end
 
+  with_options numericality: { other_than: 1, message: 'Select' } do
+  validates :category_id   
+  validates :condition_id  
+  validates :fee_id        
+  validates :prefecture_id 
+  validates :day_id        
+  end
+
   validates :price         ,numericality: { only_integer: true, message: 'Half-width number' }
   validates :price         ,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
-  validates :category_id   ,numericality: { other_than: 1, message: 'Select' }
-  validates :condition_id  ,numericality: { other_than: 1, message: 'Select' }
-  validates :fee_id        ,numericality: { other_than: 1, message: 'Select' }
-  validates :prefecture_id ,numericality: { other_than: 1, message: 'Select' }
-  validates :day_id        ,numericality: { other_than: 1, message: 'Select' }
 
 end
