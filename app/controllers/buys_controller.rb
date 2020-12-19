c7e97dee79cf1eab0c029a4027000d6a6789da08
yassunsun <ww16471z@icloud.com>
2020-12-19 15:54:27 +0900

    バリデーション

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,7 +1,16 @@
 class BuysController < ApplicationController
   def index
-    @buy = Buy.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
   end
+
+  private
+
+  def buy_params
+    params.require(:buy).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+end
--- app/models/user_buy.rb
+++ app/models/user_buy.rb
@@ -1,11 +1,22 @@
 class UserBuy
 
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture, :city, :house_number, :building, :phone_number, :buy_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :user_id, :item_id
 
-# ここにバリデーションの処理を書く
+  # ここにバリデーションの処理を書く
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
+    validates :city
+    validates :building
+    validates :phone_number, numericality: { only_integer: true, message: "Input only number"}
+  end
 
+  # 各テーブルにデータを保存する処理を書く
   def save
-    # 各テーブルにデータを保存する処理を書く
+    buy = Buy.create(user_id: user_id, item_id: item_id )
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, buy_id: buy_id)
+  end
+
   end
 end
