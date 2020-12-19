cf8997a196ff7dfc6578608ef74a9d520cd60df0
yassunsun <ww16471z@icloud.com>
2020-12-20 02:35:44 +0900

    トークン送付

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,6 +2,7 @@ function price() {
   const item_price = document.getElementById("item-price");
   const add_tax = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
+
   item_price.addEventListener("keyup", function() {
     const selling_price = item_price.value;
     let tax = Math.floor(selling_price * 0.1);
