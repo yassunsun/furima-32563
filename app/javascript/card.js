737630489f5cf581c87929360a3d62685a41b443
yassunsun <ww16471z@icloud.com>
2020-12-20 15:18:27 +0900

    Revert "JSファイルの作成"
    This reverts commit 2aedad5f06cd99a27f3e1e0ea22b644dabdaa511.

--- app/javascript/card.js
+++
@@ -1,9 +0,0 @@
-const pay = () => {
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
-  });
-};
-
-window.addEventListener("load", pay);
