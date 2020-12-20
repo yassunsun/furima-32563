a444f89af8d41f1c20aee1f249bca3cebd3bf763
yassunsun <ww16471z@icloud.com>
2020-12-20 16:30:43 +0900

    requireの追加

--- app/models/user_buy.rb
+++ app/models/user_buy.rb
@@ -6,11 +6,12 @@ class UserBuy
   # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
-    validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
     validates :city
     validates :house_number
     validates :phone_number, numericality: { only_integer: true, message: "Input only number"}
   end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
 
   # 各テーブルにデータを保存する処理を書く
   def save
